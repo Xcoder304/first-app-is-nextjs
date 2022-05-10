7294391aa0dca198027ca0e7db6b954293ce8f97
Xcoderx <xcoder304@gmail.com>
2022-05-11 00:49:18 +0500

    added the blog section

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,7 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import styles from "../styles/blogs.module.css";
+import Link from "next/dist/client/link";
 
-const blogs = () => {
-  return <div>blogs</div>;
+const blogs = (props) => {
+  const [blogs, setblogs] = useState(props.allBlogs);
+  return (
+    <div className={styles.blogs}>
+      <h1 className={styles.blogsTitle}>Blogs</h1>
+      <ul>
+        {blogs.map(({ title, content, slug }, index) => {
+          return (
+            <Link href={`/viewblog/${slug}`} key={index}>
+              <a>
+                <li>
+                  <h4>{title}</h4>
+                  <p>{content?.substr(0, 150)}...</p>
+                </li>
+              </a>
+            </Link>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
+
+export async function getServerSideProps(context) {
+  let data = await fetch("http://localhost:3000/api/allBlogs");
+  let allBlogs = await data.json();
+
+  return {
+    props: { allBlogs }, // will be passed to the page component as props
+  };
+}
 
 export default blogs;
