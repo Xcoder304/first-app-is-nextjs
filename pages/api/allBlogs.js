2c13ef5d491e281f4bda6e7aff0e1178e34084c5
Xcoderx <xcoder304@gmail.com>
2022-05-10 14:08:21 +0500

    save the all blogs to an array

---
+++ pages/api/allBlogs.js
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir(`blogdata`, "utf-8");
+  let files;
+  let blogs = [];
+  for (let i = 0; i < data.length; i++) {
+    let item = data[i];
+    files = await fs.promises.readFile(`blogdata/${item}`, "utf-8");
+    blogs.push(JSON.parse(files));
+  }
+  res.status(200).json(blogs);
+
+  //   let data = await fs.promises.readdir(`blogdata`, "utf-8", (err, data) => {
+  //     if (err) {
+  //       res.status(500).json("this is an error");
+  //     }
+  //     for (let i = 0; i < data.length; i++) {
+  //       console.log(data);
+  //     }
+  //
+  //   });
+}
